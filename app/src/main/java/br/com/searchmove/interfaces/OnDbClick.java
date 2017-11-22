dc4cf846458d25a3524adcd1420b78e9c2dd2068
Juanvvf <juanvvfreitas@gmail.com>
2017-11-21 21:44:51 -0300

    detalhes do filme

---
+++ app/src/main/java/br/com/searchmove/interfaces/OnDbClick.java
@@ -0,0 +1,11 @@
+package br.com.searchmove.interfaces;
+
+import br.com.searchmove.model.Result;
+
+/**
+ * Created by marcelo.gomes.junior on 11/19/2017.
+ */
+
+public interface OnDbClick {
+    void onDbClick(Result result);
+}
