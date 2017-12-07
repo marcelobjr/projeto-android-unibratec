db2ac86f344d00ba52942761f62b54cbd41f2166
Juanvvf <juanvvfreitas@gmail.com>
2017-12-06 22:28:43 -0300

    Activity e componentes de integrante

---
+++ app/src/main/java/br/com/searchmove/activitys/Act_ingrante2.java
@@ -0,0 +1,34 @@
+package br.com.searchmove.activitys;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import br.com.searchmove.R;
+
+public class Act_ingrante2 extends AppCompatActivity {
+
+
+    FloatingActionButton floatingActionButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_act_ingrante2);
+
+
+        View.OnClickListener tratadorDoBotao = new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent it_home = new Intent(Act_ingrante2.this, MainActivity.class);
+                startActivity(it_home);
+            }
+        };
+        findViewById(R.id.btnVoltar) .setOnClickListener(tratadorDoBotao);
+
+    }
+
+    }
+
